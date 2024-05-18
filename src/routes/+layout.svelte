b85fbd3702b4e7731003141c8627a944317f7757
IsmaelTrentin <pandux66@gmail.com>
2024-05-18 23:04:03 +0200

    setup SK + shadcn + tauri

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<div class="app">
+	<ModeWatcher />
+
+	<main>
+		<slot />
+	</main>
+</div>
+
+<!-- <slot></slot> -->
